2e43cf95e41b2b4990266948164ed2301baf1f9b
Tom <tom-vautray@hotmail.fr>
2021-03-09 22:14:56 +0100

    MainClass and skeleton of program in 3-Tier

---
+++ src/MainPackage/MainClass.java
@@ -0,0 +1,27 @@
+package MainPackage;
+
+//Program's classes
+import EntitiesPackage.*;
+
+//3-Tier
+import DataTier.*;
+import LogicTier.*;
+import ViewTier.*;
+
+public class MainClass
+{
+	public static void main(String[] args)
+	{
+		DataInterface localDataBase = new LocalDataBase();
+		DataInterface[] webScrappers = new DataInterface[1];//[2];
+		webScrappers[0] = new PixScrapper();
+		//[TODO]
+		//webScrappers[1] = new DevScrapper();
+		
+		LogicInterface logic = new Logic(localDataBase, webScrappers);
+		
+		View window = new View(logic);
+		
+		
+	}
+}
