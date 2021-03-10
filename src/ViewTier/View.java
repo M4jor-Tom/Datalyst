76a3159a45af567cc508387bf2fc6eec77d4f186
Tom <tom-vautray@hotmail.fr>
2021-03-10 17:33:16 +0100

    Neutral JFrame added

--- src/ViewTier/View.java
+++ src/ViewTier/View.java
@@ -1,14 +1,20 @@
 package ViewTier;
 
+import javax.swing.*;
 import LogicTier.LogicInterface;
 
 public class View
 {
 	private LogicInterface _logicInterface;
+	private JFrame _jframe;
 	
 	public View(LogicInterface logicInterface)
 	{
 		setLogicInterface(logicInterface);
+		setJframe(new JFrame());
+		
+		//JFrame settings
+		getJframe().setVisible(true);
 	}
 
 	public LogicInterface getLogicInterface()
@@ -19,5 +25,15 @@ public class View
 	public void setLogicInterface(LogicInterface logicInterface)
 	{
 		_logicInterface = logicInterface;
+	}
+
+	public JFrame getJframe()
+	{
+		return _jframe;
+	}
+
+	public void setJframe(JFrame jframe)
+	{
+		_jframe = jframe;
 	}
 }
