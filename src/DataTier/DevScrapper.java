a65b1302cf0440e1d0b12a7e6390cd792bda428b
Tom <tom-vautray@hotmail.fr>
2021-03-11 07:50:05 +0100

    Added DevScrapper

---
+++ src/DataTier/DevScrapper.java
@@ -0,0 +1,17 @@
+package DataTier;
+
+import java.util.ArrayList;
+
+import EntitiesPackage.Ressource;
+
+public class DevScrapper implements DataInterface
+{
+
+	@Override
+	public ArrayList<Ressource> getRessouces()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
