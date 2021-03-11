e74298102bb2f5010b346419ae1f05919ec9ca38
Tom <tom-vautray@hotmail.fr>
2021-03-11 08:16:04 +0100

    Added DevRessource

---
+++ src/EntitiesPackage/DevRessource.java
@@ -0,0 +1,18 @@
+package EntitiesPackage;
+
+import java.io.File;
+import java.net.URL;
+
+public class DevRessource extends PreReferencedRessource
+{
+	public DevRessource(int localId, URL url, File file)
+	{
+		super(localId, url, file);
+	}
+
+	@Override
+	public void setForeignIdFromUrl()
+	{
+		setForeignId(0);
+	}
+}
