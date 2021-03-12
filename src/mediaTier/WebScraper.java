50cfb1580585cd1066a153ee1e335abf36cf5c53
Tom <tom-vautray@hotmail.fr>
2021-03-12 23:57:43 +0100

    Added Cookies to WebScraper

--- src/mediaTier/WebScraper.java
+++ src/mediaTier/WebScraper.java
@@ -1,15 +1,22 @@
 package mediaTier;
 
+import java.util.HashMap;
+import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import entitiesPackage.Resource;
 
 public abstract class WebScraper implements MediaInterface
 {
 	public String _loginUrl;
 	public String _tokenName;
+	private Map<String, String> _cookies;
 	final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
 	
 	public WebScraper(String loginUrl, String tokenName, String userName, String password)
 	{
+		setCookies(null);
 		setLoginUrl(loginUrl);
 		setTokenName(tokenName);
 		login(userName, password);
@@ -43,5 +50,17 @@ public abstract class WebScraper implements MediaInterface
 	public void setTokenName(String tokenName)
 	{
 		_tokenName = tokenName;
+	}
+
+	public Map<String, String> getCookies()
+	{
+		return _cookies;
+	}
+
+	public void setCookies(Map<String, String> cookies)
+	{
+		if(cookies != null)
+			_cookies = new HashMap<>();
+		_cookies = cookies;
 	}
 }
