28c095d573796be5065de2826f2febf3512b1763
Tom <tom-vautray@hotmail.fr>
2021-03-13 21:55:43 +0100

    Added class VideoResource extends Resource

---
+++ src/EntitiesPackage/VideoResource.java
@@ -0,0 +1,12 @@
+package entitiesPackage;
+
+import java.io.File;
+import java.net.URL;
+
+public class VideoResource extends Resource
+{
+	public VideoResource(int localId, URL url, File file, String name)
+	{
+		super(localId, url, file, name);
+	}
+}
