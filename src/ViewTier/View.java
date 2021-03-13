3f2c81737a1d3f9495809c2c8d89b71fe073cc09
Tom <tom-vautray@hotmail.fr>
2021-03-13 23:29:57 +0100

    Splitted View class into itself and ImageNavigator daughter

--- src/ViewTier/View.java
+++ src/ViewTier/View.java
@@ -9,12 +9,9 @@ import javax.swing.*;
 
 import logicTier.LogicInterface;
 
-public class View
+abstract public class View
 {
 	private LogicInterface _logicInterface;
-	
-	//SWING
-	private ImagePanel _imagePanel;
 	
 	public View(LogicInterface logicInterface)
 	{
@@ -22,64 +19,7 @@ public class View
 		setLogicInterface(logicInterface);
 	}
 	
-	public void run()
-	{
-		//JFrame instantiation
-		JFrame jFrame = new JFrame();
-		
-		//Image instantiation
-		setImagePanel(new ImagePanel(getLogicInterface().getBufferedImage()));
-		
-		//JButtons instantiation
-		JButton
-			leftButton = new JButton("<"),
-			rightButton = new JButton(">");
-		
-		//JButton decoration
-		leftButton.setFocusPainted(false);
-		rightButton.setFocusPainted(false);
-		leftButton.setBackground(new Color(0, 0, 0));
-		rightButton.setBackground(new Color(0, 0, 0));
-		leftButton.setForeground(new Color(255, 255, 255));
-		rightButton.setForeground(new Color(255, 255, 255));
-		
-		//JButtons listeners
-		leftButton.addActionListener(
-			new ActionListener()
-			{
-				@Override
-				public void actionPerformed(ActionEvent e)
-				{
-					getLogicInterface().setDownCurrent();
-					getImagePanel().actualize(getLogicInterface().getBufferedImage());
-				}
-			}
-		);
-		rightButton.addActionListener(
-			new ActionListener()
-			{
-				@Override
-				public void actionPerformed(ActionEvent e)
-				{
-					getLogicInterface().setUpCurrent();
-					getImagePanel().actualize(getLogicInterface().getBufferedImage());
-				}
-			}
-		);
-		
-		//Adding Label and Panel to JFrame
-		jFrame.setLayout(new BorderLayout());
-		jFrame.getContentPane().add(getImagePanel(), BorderLayout.CENTER);//getImageLabel(), BorderLayout.CENTER);
-		jFrame.getContentPane().add(leftButton, BorderLayout.LINE_START);
-		jFrame.getContentPane().add(rightButton, BorderLayout.LINE_END);
-		
-		//JFrame settings
-		jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
-		//JFrame ready
-		jFrame.setVisible(true);
-	}
+	public abstract void run();
 
 	public LogicInterface getLogicInterface()
 	{
@@ -89,15 +29,5 @@ public class View
 	public void setLogicInterface(LogicInterface logicInterface)
 	{
 		_logicInterface = logicInterface;
-	}
-
-	public ImagePanel getImagePanel()
-	{
-		return _imagePanel;
-	}
-
-	public void setImagePanel(ImagePanel imagePanel)
-	{
-		_imagePanel = imagePanel;
 	}
 }
