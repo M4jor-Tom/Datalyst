039516c15e5e798202ce8063b7379e374a3c7114
Tom <tom-vautray@hotmail.fr>
2021-03-13 23:43:17 +0100

    Added class VideoPanel extends JPanel

---
+++ src/ViewTier/VideoPanel.java
@@ -0,0 +1,35 @@
+package viewTier;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.io.File;
+import java.io.IOException;
+
+import javax.media.CannotRealizeException;
+import javax.media.Manager;
+import javax.media.NoPlayerException;
+import javax.media.Player;
+import javax.swing.JPanel;
+
+public class VideoPanel extends JPanel
+{
+	public VideoPanel(File file)
+    {
+        setLayout(new BorderLayout());
+
+		try
+		{
+			Player mediaPlayer = Manager.createRealizedPlayer(file.toURI().toURL());
+
+	        Component video = mediaPlayer.getVisualComponent();
+	        Component controls = mediaPlayer.getControlPanelComponent();
+	        
+	        add(video,BorderLayout.CENTER);
+	        add(controls,BorderLayout.SOUTH);
+		}
+		catch(NoPlayerException | CannotRealizeException | IOException e)
+		{
+			e.printStackTrace();
+		}
+    }
+}
