436911ab6a326aa93b8f1e7ce7f06f6aecf7bb35
Tom <tom-vautray@hotmail.fr>
2021-03-14 20:59:49 +0100

    import of javafx in VideoPanel

--- src/ViewTier/VideoPanel.java
+++ src/ViewTier/VideoPanel.java
@@ -1,6 +1,14 @@
 package viewTier;
 
-import java.io.File;
+import javafx.application.Application;
+import javafx.collections.ListChangeListener;
+import javafx.collections.MapChangeListener;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaView;
+import javafx.scene.media.Track;
+import javafx.stage.Stage;
 
 public class VideoPanel extends MediaPanel
 {
