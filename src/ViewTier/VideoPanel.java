4b395b5d8e551091f32e4f84fd794e995bd0a1ad
Tom <tom-vautray@hotmail.fr>
2021-03-14 19:11:00 +0100

    Removed JFM-related code from VideoPanel

--- src/ViewTier/VideoPanel.java
+++ src/ViewTier/VideoPanel.java
@@ -1,11 +1,6 @@
 package viewTier;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
 import java.io.File;
-import java.io.IOException;
-
-import javax.media.*;
 
 public class VideoPanel extends MediaPanel
 {
@@ -13,19 +8,6 @@ public class VideoPanel extends MediaPanel
 
 	public VideoPanel(File file)
     {
-        try
-		{
-			Player mediaPlayer = Manager.createRealizedPlayer(file.toURI().toURL());
-
-	        Component video = mediaPlayer.getVisualComponent();
-	        Component controls = mediaPlayer.getControlPanelComponent();
-	        
-	        add(video, BorderLayout.CENTER);
-	        add(controls, BorderLayout.SOUTH);
-		}
-		catch(NoPlayerException | CannotRealizeException | IOException e)
-		{
-			e.printStackTrace();
-		}
+        
     }
 }
