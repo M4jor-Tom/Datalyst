9ea023fe2b300c506b7b2d3229ea3d26dfa6e77c
Tom <tom-vautray@hotmail.fr>
2021-03-14 19:05:56 +0100

    Added LogicInterface::getResource()

--- src/LogicTier/LogicInterface.java
+++ src/LogicTier/LogicInterface.java
@@ -2,10 +2,14 @@ package logicTier;
 
 import java.awt.image.BufferedImage;
 
+import entitiesPackage.Resource;
+
 public interface LogicInterface
 {
 	public void setUpCurrent();
 	public void setDownCurrent();
 
 	public BufferedImage getBufferedImage();
+
+	public Resource getResource();
 }
