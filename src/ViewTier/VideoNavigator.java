34989f17a5d5412749a7e6485d0f853242caec16
Tom <tom-vautray@hotmail.fr>
2021-03-14 19:05:01 +0100

    Added class VideoNavigator extends MediaNavigator

---
+++ src/ViewTier/VideoNavigator.java
@@ -0,0 +1,49 @@
+package viewTier;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import logicTier.LogicInterface;
+
+public class VideoNavigator extends MediaNavigator
+{
+	public VideoNavigator(LogicInterface logicInterface)
+	{
+		super(logicInterface);
+	}
+
+	@Override
+	protected void addActionListeners(JButton leftButton, JButton rightButton)
+	{
+		leftButton.addActionListener(
+				new ActionListener()
+				{
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						getLogicInterface().setDownCurrent();
+						//getImagePanel().actualize(getLogicInterface().getBufferedImage());
+					}
+				}
+			);
+		rightButton.addActionListener(
+			new ActionListener()
+			{
+				@Override
+				public void actionPerformed(ActionEvent e)
+				{
+					getLogicInterface().setUpCurrent();
+					//getImagePanel().actualize(getLogicInterface().getBufferedImage());
+				}
+			}
+		);
+	}
+
+	@Override
+	protected void instantiateMediaPanel()
+	{
+		setMediaPanel(new VideoPanel(getLogicInterface().getResource().getFile()));
+	}
+}
