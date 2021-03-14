270afd24a86de76a6238a6921ffaf84d5177acfe
Tom <tom-vautray@hotmail.fr>
2021-03-14 19:07:20 +0100

    Use of VideoNavigator instead of ImageNavigator

--- src/mainPackage/MainClass.java
+++ src/mainPackage/MainClass.java
@@ -14,18 +14,19 @@ public class MainClass
 		
 		//MEDIA-TIER
 		MediaInterface testImagesBase = new LocalDirectory("D:\\gDrive\\DCIM\\Images\\");
-		//MediaInterface testVideoBase = new LocalDirectory("D:\\gDrive\\DCIM\\Vid�os\\");
+		MediaInterface testVideoBase = new LocalDirectory("D:\\gDrive\\DCIM\\Videos\\");
 		MediaInterface[] webScrapers = new MediaInterface[2];
 		webScrapers[0] = null;//new PixScraper("[uid]", "[pwd]");
 		webScrapers[1] = null;//new DevScraper("[uid]", "[pwd]");
 		
 		//LOGIC-TIER
 		LogicInterface imageLogic = new Logic(testImagesBase, webScrapers, mysqlManager);
+		LogicInterface videoLogic = new Logic(testVideoBase, webScrapers, mysqlManager);
 		
 		//VIEW-TIER
 		View imageNavigator = new ImageNavigator(imageLogic);
-		//View videoNavigator = new VideoNavigator(videoLogic);
-		imageNavigator.run();
-		//videoNavigator.run();
+		View videoNavigator = new VideoNavigator(videoLogic);
+		//imageNavigator.run();
+		videoNavigator.run();
 	}
 }
