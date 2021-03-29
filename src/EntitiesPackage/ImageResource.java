3e7b2f9b33943aab547b3a050ff9701fe6ff7acc
Tom <tom-vautray@hotmail.fr>
2021-03-29 21:05:58 +0200

    imported & implemented Resource as a submodule

--- src/EntitiesPackage/ImageResource.java
+++ src/EntitiesPackage/ImageResource.java
@@ -3,7 +3,7 @@ package entitiesPackage;
 import java.io.File;
 import java.net.URL;
 
-public class ImageResource extends Resource
+public class ImageResource extends MediaResource
 {
 	public ImageResource(int localId, URL url, File file, String name)
 	{
--- src/EntitiesPackage/VideoResource.java
+++ src/EntitiesPackage/VideoResource.java
@@ -3,7 +3,7 @@ package entitiesPackage;
 import java.io.File;
 import java.net.URL;
 
-public class VideoResource extends Resource
+public class VideoResource extends MediaResource
 {
 	public VideoResource(int localId, URL url, File file, String name)
 	{
